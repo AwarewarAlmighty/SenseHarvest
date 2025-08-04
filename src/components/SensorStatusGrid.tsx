62c7eca84f4a117ff6158b68d90ee8d895a99a21
Muhammad Imam Alghifary <imamalghifary6@gmail.com>
2025-08-04 16:48:56 +0700

    add employee log

--- src/components/SensorStatusGrid.tsx
+++ src/components/SensorStatusGrid.tsx
@@ -33,7 +33,7 @@ interface SensorStatusGridProps {
 }
 
 const SensorStatusGrid: React.FC = () => {
-  const liveSensors = useWebSocket("ws://localhost:1880/ws/SenseHarvest/Sensors");
+  const liveSensors = useWebSocket("ws://52.65.165.101:1880/ws/SenseHarvest");
   const sensors = liveSensors ?? defaultSensors;
 
   const getStatusColor = (status: string) => {
