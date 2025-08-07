7b8c1f07ea435db84f7a39a2fda6fff081a6c266
ImamAlghifary <imamalghifary6@gmail.com>
2025-08-07 17:43:40 +0700

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -170,6 +170,7 @@ const EmployeeLogCard = () => {
 
 const InventoryInfoCard = () => {
   const websocketUrl = import.meta.env.VITE_WS_URL || "wss://senseharvest.ddns.net/ws/SenseHarvest/Sensors";
+  console.log(`This is url ${websocketUrl}`)
   const liveSensors = useWebSocket(websocketUrl);
     const sensors = Array.isArray(liveSensors) ? liveSensors : [
         { name: 'Temperature', value: 24.5, unit: '°C', status: 'normal' },
